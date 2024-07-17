6f6b3174f4033312b1b88062a334c8b54ef9c83a
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-07-17 23:20:03 +0300

    feat(api): add global error handling

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
